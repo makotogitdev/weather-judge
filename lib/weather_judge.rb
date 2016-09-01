0a0567a03843eeacfbdc12c71192557d5c35cf8e
Makoto Scott-Hinkle <makoto.scotthinkle@gmail.com>
2016-08-31 18:54:33 -0700

    Added error handling for when obtaining forecast is unsuccessful and coordinates are invalid.

--- lib/weather_judge.rb
+++ lib/weather_judge.rb
@@ -10,11 +10,26 @@ module WeatherJudge
 
     # Retrieves WeatherData object for today's forecast given latitude and longitude.
     def run(latitude, longitude)
+      validate_coordinates(latitude, longitude)
+
       ForecastIO.api_key = WeatherJudge.forecast_io_api_key
       forecast = ForecastIO.forecast(latitude, longitude, params: { exclude: 'hourly,minutely,alerts,flags'})
+
+      if forecast.nil?
+        raise StandardError.new("Unable to obtain forecast! Check your network connectivity and API key")
+      end
+
       @forecast_today = forecast.daily.data.first
 
       WeatherData.new(@forecast_today)
+    end
+
+    private
+
+    def validate_coordinates(latitude, longitude)
+      unless latitude >= -90 && latitude <= 90 && longitude >= -180 && longitude <= 180
+        raise ArgumentError.new("Invalid coordinates!")
+      end
     end
   end
 end
--- spec/weather_judge_spec.rb
+++ spec/weather_judge_spec.rb
@@ -2,24 +2,41 @@ require 'spec_helper'
 
 describe WeatherJudge do
   describe '.run' do
-    before do
-      todays_forecast = double("TodaysForecast")
-      daily_forecast_data = double("DailyForecastData", :first => todays_forecast)
-      daily_forecast = double("DailyForecast", :data => daily_forecast_data)
-      forecast_data = double("Forecast", :daily => daily_forecast)
+    context 'when retrieving forecast is successful' do
+      before do
+        todays_forecast = double("TodaysForecast")
+        daily_forecast_data = double("DailyForecastData", :first => todays_forecast)
+        daily_forecast = double("DailyForecast", :data => daily_forecast_data)
+        forecast_data = double("Forecast", :daily => daily_forecast)
 
-      allow(ForecastIO).to receive(:forecast).and_return(forecast_data)
+        allow(ForecastIO).to receive(:forecast).and_return(forecast_data)
+      end
+
+      subject { WeatherJudge.run(44, -125) }
+
+      it 'calls to get forecast' do
+        subject
+        expect(ForecastIO).to have_received(:forecast).once
+      end
+
+      it 'returns WeatherData' do
+        expect(subject).to be_instance_of(WeatherJudge::WeatherData)
+      end
     end
 
-    subject { WeatherJudge.run(44, -125) }
+    context 'when retrieving forecast failed' do
+      before do
+        allow(ForecastIO).to receive(:forecast).and_return(nil)
+      end
 
-    it 'calls to get forecast' do
-      subject
-      expect(ForecastIO).to have_received(:forecast).once
-    end
+      it 'raises ArgumentError when coordinates are invalid' do
+        expect{ WeatherJudge.run(100, -125) }.to raise_error(ArgumentError, "Invalid coordinates!")
+      end
 
-    it 'returns WeatherData' do
-      expect(subject).to be_instance_of(WeatherJudge::WeatherData)
+      it 'raises an exception when no forecast data is returned with valid coordinates' do
+        expect{ WeatherJudge.run(44, -125) }.to raise_error(StandardError, "Unable to obtain forecast! "\
+          "Check your network connectivity and API key")
+      end
     end
   end
 end
